c0d4628def03991f9531cc4a64979d58ea93857b
ashok-kujur <ashok@cidev.in>
2026-01-19 17:37:15 +0530

    add server

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "vgroupapp",
+      script: "npm",
+      args: "start",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3045,
+      },
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    },
+  ],
+};
